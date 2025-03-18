7e7d15e307786b23043c2e8d8e9ca0564e9d5656
u <u>
2025-03-18 14:04:43 +0300

    update

--- copr_gui/__dynamic/builds_monitor.py
+++ copr_gui/__dynamic/builds_monitor.py
@@ -157,7 +157,6 @@ class BuildsMonitor(MonitorCommon):
             proxy.cancel(item)
         except Exception:
             pass
-        print(item)
         return proxy.delete_list([item])
     
     def __init__(self, parent, config_args, filter_args=None,**kwargs):
