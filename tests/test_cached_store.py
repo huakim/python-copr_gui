b3221faef1b19bb5365f65747fb2dbf575aa9074
u <u>
2025-03-25 22:22:27 +0300

    update

--- tests/test_cached_store.py
+++ tests/test_cached_store.py
@@ -4,8 +4,30 @@ from time import time, sleep
 
 
 class ComplicatedCache:
-    def get_time(self):
+    def get_time(self, arg=''):
         return time()
+
+
+def test_with_argument():
+    cc = ComplicatedCache()
+    cache = cs.CallCache()
+    mock1 = cs.CallCacheMock(cc, cache)
+    mock2 = cs.CallCacheMock(cc, cache)
+    mock3 = cs.CallCacheMock(cc, cache)
+    mock4 = cs.CallCacheMock(cc, cache)
+    t1 = mock1.get_time("F")
+    sleep(0.1)
+    t2 = mock2.get_time("F")
+    sleep(0.1)
+    t3 = mock3.get_time("F")
+    sleep(0.1)
+    t4 = mock4.get_time("D")
+    sleep(0.1)
+    t5 = mock3.get_time("D")
+    assert t1 == t2
+    assert t1 == t3
+    assert t1 != t4
+    assert t4 == t5
 
 
 def test_simple_cache():
